7571a5695dad279fe226e39858fc34c8c3ebde5d
leonidseoshnik <leonseoprofi@gmail.com>
2026-03-09 14:29:46 +0200

    add quiz.py

--- quiz.py
+++ quiz.py
@@ -5,9 +5,11 @@ with open('quiz.txt', 'w+', encoding='utf-8') as file:
     file.seek(0)
     result = 0
     data = file.readlines()
+
     for line in data:
         temp = line.split('?')
         answer = input(f'{temp[0]}?\n')
         if answer.strip().lower() == temp[1].strip().lower():
             result += 1
+
     print(f'Ваш счет: {result}')
