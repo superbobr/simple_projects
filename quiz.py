676c9289c79c36b6699f5fbae2aa18e58dbd2cb0
leonidseoshnik <leonseoprofi@gmail.com>
2026-03-09 14:28:47 +0200

    add quiz.py

---
+++ quiz.py
@@ -0,0 +1,13 @@
+with open('quiz.txt', 'w+', encoding='utf-8') as file:
+    file.write('Столица Франции? Париж' + '\n')
+    file.write('2 + 2 * 2? 6' + '\n')
+    file.write('Самая длинная река в мире? Амазонка' + '\n')
+    file.seek(0)
+    result = 0
+    data = file.readlines()
+    for line in data:
+        temp = line.split('?')
+        answer = input(f'{temp[0]}?\n')
+        if answer.strip().lower() == temp[1].strip().lower():
+            result += 1
+    print(f'Ваш счет: {result}')
