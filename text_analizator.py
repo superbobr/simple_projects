796f35f9f0c948fbdd7d4bff9cf390c88a10f116
leonidseoshnik <leonseoprofi@gmail.com>
2026-03-09 12:46:14 +0200

    end text-analizator project

--- text_analizator.py
+++ text_analizator.py
@@ -1,3 +1,19 @@
 text = '''Это первая строка для анализа.
 А это уже вторая строка.
 И, наконец, третья.'''
+
+with open('text_for_analysis.txt', 'w+', encoding='utf-8') as file:
+    file.write(text)
+    file.seek(0)
+    data = file.readlines()
+    rows = len(data)
+    words = 0
+    chars = 0
+    for line in data:
+        words += len(line.split())
+        chars += len(line)
+
+print(
+f'''Символов: {chars}
+Слов: {words}
+Строк: {rows}''')
